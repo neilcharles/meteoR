3ac571ba2e7ae5e37649ca50b3af02d3f95324e8
Neil Charles <neil.charles@mediacom.com>
2018-03-05 21:22:37 +0000

    added caching feature to find_closest_site()

--- R/find_closest_site.R
+++ R/find_closest_site.R
@@ -5,15 +5,22 @@
 #' @param long Decimal longitude
 #' @param observations \code{Boolean}. \code{FALSE} (default) returns a forecast location. \code{TRUE} returns a past 24 hour observations location.
 #' @param site_count Number of sites to return. Defaults to 1 but may be increased if data from the closest site presents problems.
+#' @param cached_sites Optionally pass output from a \code{forecast_list_sites()} call or \code{observations_list_sites()} to the function. If not included, a site list will be requested from the api.
 #' @export
 #' @examples
 #' find_closest_site(lat = 53.8059821, long = -1.6057714, observations = TRUE, site_count = 5)
 #' @importFrom magrittr %>%
-find_closest_site <- function(lat, long, observations = FALSE, site_count = 1) {
-  if (observations) {
-    sites <- observations_list_sites()
-  } else {
-    sites <- forecast_list_sites()
+find_closest_site <- function(lat, long, observations = FALSE, site_count = 1, cached_sites = NULL) {
+
+  if(!is.null(cached_sites)){
+    sites <- cached_sites
+  } else{
+
+    if (observations) {
+      sites <- observations_list_sites()
+    } else {
+      sites <- forecast_list_sites()
+    }
   }
 
   sites_distance <- sites %>%
