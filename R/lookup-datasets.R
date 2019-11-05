5b21d2ae2fce7f32c7a9a961a3a70f4e5b79c1bd
Neil Charles <neil.charles@mediacom.com>
2019-11-05 16:19:25 +0000

    Cleaned up exporting of /data

---
+++ R/lookup-datasets.R
@@ -0,0 +1,18 @@
+#' Weather type lookup
+#'
+#' @docType data
+#'
+#' @usage data(weather_type_lookup)
+
+#' @examples
+#' data(weather_type_lookup)
+"weather_type_lookup"
+#' Visibility lookup
+#'
+#' @docType data
+#'
+#' @usage data(visibility_lookup)
+
+#' @examples
+#' data(visibility_lookup)
+"visibility_lookup"
